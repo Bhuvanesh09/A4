9ceab46e88f9f76d9091252e05e363a29a27bd41
Bhuvanesh <bhuvanesh.iiith@gmail.com>
2019-04-19 16:43:24 +0530

    Tests Modified

--- desencrypt/des3/tests.py
+++ desencrypt/des3/tests.py
@@ -5,6 +5,7 @@ from .models import Quiz
 class QuizCheck(TestCase):
     """Checks if the database is runnning well and nicely and returning the questions along with their answers"""
     def returnsQuestion(self):
+        o = Quiz(q_id = 1 ,q_text = "question" , option1 = "a" , option2 = "b" , option3 = "c" , option4="d" , answer = "a")
         objectQuestion = Quiz.objects.all() 
         print(len(objectQuestion))
         self.assertIs(len(objectQuestion)>0,True)
