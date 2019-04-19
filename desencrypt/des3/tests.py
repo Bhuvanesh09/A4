0a2d6d9329accb22545901aa86cde19306a7a833
Bhuvanesh <bhuvanesh.iiith@gmail.com>
2019-04-19 16:35:10 +0530

    Added a test

--- desencrypt/des3/tests.py
+++ desencrypt/des3/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Quiz
 # Create your tests here.
+
+class QuizCheck(TestCase):
+    """Checks if the database is runnning well and nicely and returning the questions along with their answers"""
+    def returnsQuestion(self):
+        objectQuestion = Quiz.objects.all() 
+        print(len(objectQuestion))
+        self.assertIs(len(objectQuestion)>0,True)
